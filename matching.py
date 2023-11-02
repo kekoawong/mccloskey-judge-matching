8e44dfaf82862a1e27290c87e521e2649a0c889b
kekoawong <kwong6@nd.edu>
2023-11-02 16:52:24 -0400

    Rewriting matching function

--- matching.py
+++ matching.py
@@ -14,7 +14,83 @@ def get_next_category(categories={}):
     max_ratio_key = max(ratios, key=lambda k: ratios[k])
     return max_ratio_key
 
-def match_judges(judges={}, companies={}, categories={}, min_company_judges=8, max_judge_companies=10):
+
+def match_judges(judges={}, companies={}, categories={}, max_judge_companies=10):
+    '''
+    Function will take in the judges, companies, and categories \n
+        judges = { judge_name: [judge_categories] } \n
+        companies = { company_name: company_category } \n
+        categories = { category: { num_companies: number; num_judges: number; } } \n
+    Will output the following list of lists. \n
+    Judge List: [judge_name, [categories], [companies], [companies-with-reasoning]] \n
+    Company List: [company_name, category, queue_number, queue_number_with_reasoning, [judges], [judges-with-reasoning]]
+    '''
+    # set constraints
+    total_judges = len(judges.keys())
+    total_companies = len(companies.keys())
+    min_judges = (total_judges * max_judge_companies) / total_companies
+
+    # return objects
+    return_judges = {}
+    return_companies = {}
+
+    # define adding a judge to a company
+    def add_judge_to_company(judge, company, within_category=True):
+        # add company to judge database
+        if judge not in return_judges:
+            return_judges[judge] = { "companies": [], "companies_with_reasoning": [] }
+        return_judges[judge]["companies"].append(f'{company if within_category else company + " - Fill-in"}')
+        return_judges[judge]["companies_with_reasoning"].append(f'{company} - {next_category if within_category else " - Fill-in"}.')
+        
+        # add judge to company database
+        if company not in return_companies:
+            return_companies[company] = { "queue_number": 0, "queue_number_with_reasoning": "", "judges": [], "judges_with_reasoning": [] }
+        return_companies[company]["judges"].append(f'{judge if within_category else judge + " - Fill-in"}')
+        return_companies[company]["judges_with_reasoning"].append(f'{judge} - {next_category if within_category else " - Fill-in"}. All interests are {", ".join(judges[judge])}')
+
+        # remove judge from being available if hits max number
+
+        return
+
+    # loop through the companies, make a list of companies in each categories
+    company_categories = {}
+    for name, cat in companies.items():
+        if cat not in company_categories:
+            company_categories[cat] = []
+        company_categories[cat].append(name)
+
+    # loop through and add the judges to each company
+    remaining_companies = len(companies.keys())
+    while remaining_companies > 0:
+        # get next category and company
+        next_category = get_next_category(categories=categories)
+        next_company = company_categories[next_category].pop(0)
+
+        # set queue variables
+        queue_num = len(companies.keys()) - remaining_companies
+        queue_str = f'{queue_num} since {categories[next_category]["num_companies"]} {next_category} companies and {categories[next_category]["num_judges"]} available judges'
+        
+        # decrement companies in that category
+        categories[next_category]["num_companies"] -= 1
+
+        # assign variables for judge loop
+        judges_shuffled = list(random.sample(judges.keys(), len(judges)))
+        num_extra_judges_needed = min_judges - categories[next_category]["num_judges"]
+
+        # loop through judges
+        for judge in judges_shuffled:
+            # if the judge is available and has the category, assign to company
+            if judge in judges and next_category in judges[judge]:
+                pass
+            # if judge is available and company needs a random judge, assign to company
+            elif judge in judges and num_extra_judges_needed > 0:
+                pass
+
+            # break from loop if num judges met
+            
+    
+
+def match_judges_old(judges={}, companies={}, categories={}, max_judge_companies=10):
     '''
     Function will take in the judges, companies, and categories \n
     Will output the following list of lists. \n
